fc8b1c6a26d05ec398410f494d95d4283405e70d
Stanislav Suzdalev <stasuz@mail.ru>
2024-02-04 22:03:12 +0300

    Add plugins: Comment.nvim; auto-save.nvim (+ configured to save rust files only)

--- nvim/lua/rocketman/packer.lua
+++ nvim/lua/rocketman/packer.lua
@@ -72,4 +72,30 @@ return require('packer').startup(function(use)
 	use "tpope/vim-sleuth"
 
 	use 'Exafunction/codeium.vim'
+
+	use {
+		'numToStr/Comment.nvim',
+		config = function()
+			require('Comment').setup()
+		end
+	}
+
+	use({
+		"Pocco81/auto-save.nvim",
+		config = function()
+			require("auto-save").setup {
+				 condition = function(buf)
+					local fn = vim.fn
+					local utils = require("auto-save.utils.data")
+
+					if
+						fn.getbufvar(buf, "&modifiable") == 1 and
+						not utils.not_in(fn.getbufvar(buf, "&filetype"), { "rust" }) then
+						return true -- met condition(s), can save
+					end
+					return false -- can't save
+				end,
+			}
+		end,
+	})
 end)
