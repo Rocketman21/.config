7d80fd26274306185429c0272204b82f8e309be5
Stanislav Suzdalev <stasuz@mail.ru>
2024-12-04 15:09:54 +0300

    Tmux select-window by Alt+12345

--- nvim/after/plugin/jdtls.lua
+++ nvim/after/plugin/jdtls.lua
@@ -2,4 +2,4 @@ local config = {
     cmd = {'/usr/local/bin/jdtls'},
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
-require('jdtls').start_or_attach(config)
+-- require('jdtls').start_or_attach(config)
