3879cf71c23256192a5b2fd4ec329476e344db6f
Stanislav Suzdalev <stasuz@mail.ru>
2025-01-13 18:49:44 +0300

    Fix more tsserver -> ts_ls

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -24,8 +24,8 @@ require('mason-lspconfig').setup({
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
     end,
-    tsserver = function()
-      require('lspconfig').tsserver.setup({
+    ts_ls = function()
+      require('lspconfig').ts_ls.setup({
         init_options = {
           preferences = {
             importModuleSpecifierPreference = 'non-relative',
