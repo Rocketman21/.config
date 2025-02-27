02d163f88c35024574b7cf39071bdfbbff15f801
Stanislav Suzdalev <stasuz@mail.ru>
2025-02-27 17:44:55 +0300

    Language layout vim/alacritty improvements

--- nvim/lua/rocketman/plugins/telescope.lua
+++ nvim/lua/rocketman/plugins/telescope.lua
@@ -1,13 +1,10 @@
 return {
 	'nvim-telescope/telescope.nvim',
-	tag = '0.1.4',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		local telescope = require('telescope')
 		local builtin = require('telescope.builtin')
 		local utils = require("telescope.utils")
-
-		telescope.load_extension('harpoon')
 
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
--- nvim/lua/rocketman/set.lua
+++ nvim/lua/rocketman/set.lua
@@ -1,5 +1,9 @@
 vim.opt.guicursor = ""
 
+vim.opt.keymap = "russian-jcukenwin"
+vim.opt.iminsert = 0
+vim.opt.imsearch = 0
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
