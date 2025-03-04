0da8f9d13f4b039de9838c86875cb7fac6759b32
Stanislav Suzdalev <stasuz@mail.ru>
2025-03-05 02:26:16 +0300

    Set a directory for orgmode archive

--- nvim/lua/rocketman/plugins/orgmode.lua
+++ nvim/lua/rocketman/plugins/orgmode.lua
@@ -4,8 +4,9 @@ return {
   config = function()
     -- Setup orgmode
     require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
+      org_agenda_files = '/Users/Shared/orgfiles/**/*',
+      org_default_notes_file = '/Users/Shared/orgfiles/refile.org',
+      org_archive_location = './archive/%s_archive::'
     })
   end,
 }
